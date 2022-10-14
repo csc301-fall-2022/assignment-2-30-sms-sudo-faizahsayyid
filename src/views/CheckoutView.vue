504e53e0159f6f7662741ac5fab0b3ffd2a22e23
sms-sudo <irock12022@gmail.com>
2022-10-14 17:07:44 -0400

    feat: testing changing page

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
-        <h1>Checkout</h1>
-        <!-- TODO: Add UI for this -->
+        <h1 class="h3 mb-4 text-right">Your Shopping Cart</h1>
         <div>{{ JSON.stringify(checkoutCart) }}</div>
+        <div class="w-75 mx-auto">
+          <ProductCard v-for="product in products" 
+            :checkoutCart='checkoutCart' 
+            @add-to-cart="$emit('add-to-cart', product.id)" 
+            @increase-quantity="$emit('increase-quantity', product.id)"
+            @decrease-quantity="$emit('decrease-quantity', product.id)"
+            v-bind:key="product.id" 
+            :product="product"/>
+      </div>
     </div>
 </template>
 
 <script>
+
 export default {
     name: 'CheckoutView',
     props: {
         checkoutCart: Array,
-    }
+    },
+    async created() {
+      this.products = await getAllProducts();
+    },
+    emits: ['add-to-cart', 'increase-quantity', 'decrease-quantity']
 }
 </script>
